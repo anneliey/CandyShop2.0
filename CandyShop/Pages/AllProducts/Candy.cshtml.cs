65456cdd2a93c9835946083493af1cc313708d18
anneliey <annelieyan@gmail.com>
2021-11-16 20:21:48 +0100

    update

--- CandyShop/Pages/AllProducts/Candy.cshtml.cs
+++ CandyShop/Pages/AllProducts/Candy.cshtml.cs
@@ -6,12 +6,15 @@ using CandyShop.Data;
 using CandyShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CandyShop.Pages
 {
     public class CandyProductsModel : PageModel
     {
         public IEnumerable<ProductModel> CandyProductView { get; set; } = new List<ProductModel>();
+        public IEnumerable<ProductModel> CandyDbProductView { get; set; } = new List<ProductModel>();
+
         [BindProperty]
         public string SearchTerm { get; set; }
         private readonly CandyShop.Data.ProductContext _context;
@@ -24,9 +27,14 @@ namespace CandyShop.Pages
         public void OnGet()
         {
             List<ProductModel> products = ProductManager.GetProducts();
+            DbSet<ProductModel> dbProducts = _context.Products;
            
             var result = products.Where(product => product.Category.Contains("Candy")).ToList();
             CandyProductView = result;
+
+
+            var dbResult = dbProducts.Where(product => product.Category.Contains("Candy")).ToList();
+            CandyDbProductView = dbResult;
         }
         public void OnPost()
         {

ba28347f49728f2fd9a0a912dfd69cd30268e2e8
Chiltrin <s.s.ilic@hotmail.com>
2021-11-17 18:23:13 +0100

    Fixed merge conflict

--- CandyShop/Pages/OrderConfirmation.cshtml.cs
+++ CandyShop/Pages/OrderConfirmation.cshtml.cs
@@ -20,13 +20,9 @@ namespace CandyShop.Pages
         public double totalSum { get; set; }
         public List<Models.ProductModel> CartProducts { get; private set; }
 
-<<<<<<< Updated upstream
         public void OnGet(string name, string email, string number, string adress, string city, string postalcode, 
             string shippingOption, string paymentOption)
-=======
-        public void OnGet(string name, string email, string number, string adress, string city, 
-            string postalcode, string shippingOption, string paymentOption)
->>>>>>> Stashed changes
+
         {
             Name = name;
             Email = email;
@@ -39,10 +35,6 @@ namespace CandyShop.Pages
 
             totalSum = CartModel.totalSum;
             CartProducts = CartModel.CartProducts;
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
         }
     }
 }
